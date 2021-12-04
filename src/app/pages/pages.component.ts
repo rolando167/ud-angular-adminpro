00f3d02b823f1f3e8787eccee146d5c27023ada3
Rolando HR <rolando167@hotmail.com>
2021-12-04 12:36:56 -0500

    cambios

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunction():any;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -13,7 +15,7 @@ export class PagesComponent implements OnInit {
   constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
-
+    customInitFunction();
   }
 
 }

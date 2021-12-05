5120007db597cd4bf761b9652f478da51b8bb706
Rolando HR <rolando167@hotmail.com>
2021-12-05 17:07:12 -0500

    cambios

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivationEnd, Event, Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Event, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -8,29 +9,35 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
   public titulo: string = '';
+  public tituloSubs$: Subscription;
 
-  constructor(private router: Router) {
-    this.getDataRutas();
+  constructor(private router: Router, private route:ActivatedRoute) {
+    // console.log(route.snapshot.data);
+
+    this.tituloSubs$ = this.getDataRutas()
+                        .subscribe(({titulo}) => {
+                        this.titulo = titulo;
+                        document.title = `AdminPro - ${titulo}`;
+                      });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
   getDataRutas(){
-    this.router.events
+
+    return this.router.events
       .pipe(
         // filter( event => event instanceof  ActivationEnd ),
         filter((event: Event): event is ActivationEnd => event instanceof ActivationEnd),
         filter( (event ) => event.snapshot.firstChild === null ),
         map( (event ) => event.snapshot.data ),
-      )
-      .subscribe(({titulo}) => {
-        this.titulo = titulo;
-        document.title = `AdminPro - ${titulo}`;
-      })
+      );
   }
 
-  ngOnInit(): void {
-  }
+  // https://stackoverflow.com/questions/57552863/angular-router-event
 
 }

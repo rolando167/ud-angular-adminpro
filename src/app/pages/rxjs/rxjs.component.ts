83fd6c8588bcc11e6f1bad708c74fa668540b502
Rolando HR <rolando167@hotmail.com>
2021-12-05 16:16:55 -0500

    cambios

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy} from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { retry, take, map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-rxjs',
@@ -7,11 +8,57 @@ import { Observable } from 'rxjs';
   styles: [
   ]
 })
-export class RxjsComponent  {
+export class RxjsComponent implements OnDestroy {
+
+  public intervalSubs: Subscription ;
 
   constructor() {
-    const obs$ = new Observable(observer =>{
-      let i = -1;
+
+    // this.retornaObservable().pipe(
+    //   retry(1)
+    // )
+    // .subscribe(
+    //   valor => console.log('Subs:', valor),
+    //   error => console.log('Subs:', error),
+    //   () => console.log('Obs Terminado !!'),
+    // );
+    this.intervalSubs = this.retornaIntervalo().subscribe( console.log )
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
+  }
+
+  retornaIntervalo(): Observable<number>{
+
+    return interval(1000)
+            .pipe(
+              map( valor => valor + 1 ),
+              filter( valor => ( valor % 2 === 0)? true: false),
+              // take(10),
+            );
+  }
+
+  retornaIntervalo0(): Observable<number>{
+
+    const intervalo$ = interval(1000)
+                        .pipe(
+                          map( valor => {
+                            return valor + 1;
+                            // return 'Hola mundo ' +valor;
+                          }),
+                          filter( valor => ( valor % 2 === 0)? true: false),
+                          take(10),
+
+                        );
+
+    return intervalo$;
+  }
+
+  retornaObservable(): Observable<number>{
+    let i = -1;
+
+    const obs$ = new Observable<number>(observer =>{
 
       const intervalo = setInterval( () =>{
         i++;
@@ -31,13 +78,7 @@ export class RxjsComponent  {
 
     });
 
-    obs$.subscribe(
-      valor => console.log('Subs:', valor),
-      error => console.log('Subs:', error),
-      () => console.log('Obs Terminado !!'),
-    );
+    return obs$;
   }
-
-
 
 }

9564e282c09b2d380ab3808c0ca815f2af7e8d61
Rolando HR <rolando167@hotmail.com>
2021-12-05 12:05:54 -0500

    ca

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -11,20 +11,33 @@ export class PromesasComponent implements OnInit {
 
   ngOnInit(): void {
 
-    const promesa = new Promise( (resolve, reject) => {
-      if(true){
-        resolve('Hola Mundo 😄');
-      }else{
-        reject('Upps Algo salió mal!! 🛑')
-      }
+    // const promesa = new Promise( (resolve, reject) => {
+    //   if(true){
+    //     resolve('Hola Mundo 😄');
+    //   }else{
+    //     reject('Upps Algo salió mal!! 🛑')
+    //   }
+    // });
+
+    // promesa.then( (mensaje)=>{
+    //   console.log(mensaje);
+    // }).catch( error => console.log('Error en mi promesa', error));
+
+    // console.log('Fin del Init');
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
+    });
+  }
+
+  getUsuarios(){
+
+    const promesa = new Promise(resolve =>{
+      fetch('https://reqres.in/api/users')
+        .then( resp => resp.json() )
+        .then( body => resolve( body.data ) );
     });
 
-    promesa.then( (mensaje)=>{
-      console.log(mensaje);
-    }).catch( error => console.log('Error en mi promesa', error));
-
-    console.log('Fin del Init');
-
+    return promesa;
   }
 
 }
